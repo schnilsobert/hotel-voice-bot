7279244f53513df8b6d120b88c025f8e8f827ec9
schnilsobert <nils.friedrich247@gmail.com>
2026-03-18 18:56:51 +0100

    add auto-deploy workflow

--- responses.py
+++ responses.py
@@ -1,18 +1,35 @@
-WELCOME = """<speak><prosody rate="85%">Willkommen bei Schreinerei Hartung in Fulda.<break time="600ms"/> 
-Wir sind gerade nicht erreichbar.<break time="400ms"/> Für unsere Öffnungszeiten drücken Sie die 1.<break 
-time="500ms"/> Um eine Rückrufbitte zu hinterlassen drücken Sie die 2.<break time="500ms"/> 
-Um eine Nachricht zu hinterlassen drücken Sie die 3.<break time="500ms"/>
- Um mit jemandem verbunden zu werden drücken Sie die 9.</prosody></speak>"""
+WELCOME = """<speak><prosody rate="85%">Willkommen bei Schreinerei Hartung in Fulda.<break time="600ms"/>
+Bitte beachten Sie, dass Nachrichten zur Bearbeitung aufgezeichnet werden können.<break time="600ms"/> 
+Wir sind gerade nicht erreichbar.<break time="400ms"/> 
+Für unsere Betriebszeiten drücken Sie die 1<break time="500ms"/> 
+Für einen schnellen Rückruf drücken Sie die 2<break time="500ms"/> 
+Um Ihr Projekt oder Anliegen zu schildern drücken Sie die 3<break time="500ms"/>
+Um mit jemandem verbunden zu werden drücken Sie die 9</prosody></speak>"""
 
-OPENING_HOURS = """<speak><prosody rate="85%">Unsere Öffnungszeiten sind Montag bis Freitag von 7 bis 17 Uhr,
-<break time="300ms"/> sowie Samstag von 8 bis 12 Uhr.</prosody></speak>"""
+OPENING_HOURS = """<speak><prosody rate="85%">Unsere Betriebszeiten sind Montag bis Freitag von 7 bis 17 Uhr,<break time="300ms"/> sowie Samstag von 8 bis 12 Uhr.</prosody></speak>"""
 
-CALLBACK_MESSAGE = """<speak><prosody rate="85%">Bitte sprechen Sie Ihren Namen und Ihre Rückrufnummer nach dem Ton.
-<break time="300ms"/> Drücken Sie die Raute-Taste wenn Sie fertig sind.</prosody></speak>"""
+CALLBACK_MESSAGE = """<speak><prosody rate="85%">
+Bitte nennen Sie nach dem Ton Ihren Namen und Ihre Rückrufnummer.<break time="400ms"/>
+Wir melden uns schnellstmöglich bei Ihnen zurück.<break time="400ms"/>
+Legen Sie danach auf oder drücken Sie die Raute-Taste wenn Sie fertig sind.
+</prosody></speak>"""
 
-VOICEMAIL_MESSAGE = """<speak><prosody rate="85%">Bitte hinterlassen Sie Ihre Nachricht nach dem Ton.
-<break time="300ms"/> Drücken Sie die Raute-Taste wenn Sie fertig sind.</prosody></speak>"""
+VOICEMAIL_MESSAGE = """<speak><prosody rate="85%">
+Bitte schildern Sie nach dem Ton Ihr Projekt oder Anliegen.<break time="400ms"/>
+Nennen Sie gerne auch Ihren Namen, Ihre Rückrufnummer und einen passenden Rückrufzeitraum.<break time="400ms"/>
+Wir melden uns so schnell wie möglich bei Ihnen.<break time="400ms"/>
+Legen Sie danach auf oder drücken Sie die Raute-Taste wenn Sie fertig sind.
+</prosody></speak>"""
 
 FORWARD_MESSAGE = """<speak><prosody rate="85%">Einen Moment bitte,<break time="300ms"/> ich verbinde Sie weiter.</prosody></speak>"""
 
 INVALID_INPUT = """<speak><prosody rate="85%">Ungültige Eingabe.<break time="300ms"/> Bitte versuchen Sie es erneut.</prosody></speak>"""
+
+WELCOME_CLOSED = """<speak><prosody rate="85%">Willkommen bei Schreinerei Hartung in Fulda.<break time="600ms"/>
+Bitte beachten Sie, dass Nachrichten zur Bearbeitung aufgezeichnet werden können.<break time="600ms"/>
+Wir sind aktuell außerhalb unserer Betriebszeiten.<break time="400ms"/>
+Für einen schnellen Rückruf drücken Sie die 2<break time="500ms"/>
+Um Ihr Projekt oder Anliegen zu schildern drücken Sie die 3</prosody></speak>"""
+
+CLOSED_FORWARD = """<speak><prosody rate="85%">Wir sind aktuell außerhalb unserer Betriebszeiten.<break time="400ms"/>
+Bitte hinterlassen Sie eine Nachricht,<break time="300ms"/> wir melden uns am nächsten Werktag bei Ihnen.</prosody></speak>"""
